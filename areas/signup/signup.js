dbae9c6610a16eb973e597b944eb63bc8a83c1a0
majorbyte <majorbyte@gmail.com>
2018-05-23 00:47:32 +0200

    Login and signup implemented
    crude signup funcionality in place

---
+++ areas/signup/signup.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const express = require('express')
+  , signupService = require('../../lib/signupService')
+  , router = express.Router();
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  req.session.returnUrl = req.baseUrl;
+  res.redirect('/account/login');
+}
+
+
+router.get('/', ensureAuthenticated, async function(req, res){
+  let user = await signupService.getExistingTeam(req.user.name);
+  let signup = await signupService.getSignup(req.user.name);
+
+  if (signup){
+    signup.signedUp = true;
+  }
+  res.render('signup/overview', { user: signup || user || {reddit: req.user.name, isNew :true} });
+});
+
+router.get('/change', ensureAuthenticated, async function(req, res){
+  let user = await signupService.getExistingTeam(req.user.name);
+  let signup = await signupService.getSignup(req.user.name);
+
+  if(!signup && user){
+    res.render('signup/signup-existing', { user: user});
+    return;
+  }
+
+  switch(signup.saveType){
+    case "existing":
+      res.render('signup/signup-existing', { user: signup || user});
+      break;
+    case "reroll":
+      res.render('signup/signup-reroll', {user: signup});
+      break;
+    case "new":
+      res.render('signup/signup-new-coach', {user: signup});
+      break;
+    default:
+      res.render('signup/signup-new-coach', {user: req.user.name});
+      break;
+  }
+});
+
+router.get('/reroll', ensureAuthenticated, async function(req, res){
+  let user = await signupService.getExistingTeam(req.user.name);
+  let signup = await signupService.getSignup(req.user.name);
+  if (user) {
+    user.team = "";
+    user.race = "";
+    res.render('signup/signup-reroll', { user: signup || user });
+  }
+  else {
+    res.render('signup/signup-new-coach', { user: signup });
+  }
+
+});
+
+
+
+router.post('/confirm-existing', ensureAuthenticated, async function(req, res){
+
+  //remove unwanted input
+  delete req.body.coach;
+  delete req.body.team;
+
+  req.body.saveType = "existing";
+  await signupService.saveSignup(req.user.name, req.body);
+
+  res.redirect('/signup');
+});
+
+router.post('/confirm-reroll', ensureAuthenticated, async function(req, res){
+  //remove unwanted input
+  delete req.body.coach;
+
+  req.body.saveType = "reroll";
+  let user = await signupService.saveSignup(req.user.name, req.body);
+
+  if (user.error){
+    res.render('signup/signup-reroll', {user: user});
+  } else {
+    res.render('signup/signup-confirmed-greenhorn', {user: user});
+  }
+});
+
+router.post('/confirm-new', ensureAuthenticated, async function(req, res){
+  req.body.saveType = "new";
+  let user = await signupService.saveSignup(req.user.name, req.body);
+
+  if (user.error){
+    res.render('signup/signup-new-coach', {user: user});
+  } else {
+    res.render('signup/signup-confirmed-greenhorn', {user: user});
+  }
+});
+
+router.post('/confirm-greenhorn', ensureAuthenticated, async function(req, res){
+  let user = await signupService.saveGreenhornSignup(req.user.name);
+
+  res.redirect('/signup');
+});
+
+
+router.post('/resign', ensureAuthenticated, async function(req,res){
+  await signupService.resign(req.user.name);
+  res.redirect('/signup');
+});
+
+router.post('/resign-greenhorn', ensureAuthenticated, async function(req,res){
+  await signupService.resignGreenhorn(req.user.name);
+  res.redirect('/signup');
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,9 +17,58 @@ if (process.env.NODE_ENV === 'production'){
 
 const express = require('express')
   , path = require('path')
-  , fs = require('fs');
+  , fs = require('fs')
+  , passport = require('passport')
+  , crypto = require('crypto')
+  , util = require('util')
+  , session = require('express-session')
+  , methodOverride = require('method-override')
+  , bodyParser = require("body-parser")
+  , NedbStore = require('connect-nedb-session')(session)
+  , RedditStrategy = require('passport-reddit').Strategy;
+
+const REDDIT_CONSUMER_KEY = process.env['redditKey'];
+const REDDIT_CONSUMER_SECRET = process.env['redditSecret'];
 
 const app = module.exports = express();
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Reddit profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the RedditStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Reddit
+//   profile), and invoke a callback with a user object.
+passport.use(new RedditStrategy({
+    clientID: REDDIT_CONSUMER_KEY,
+    clientSecret: REDDIT_CONSUMER_SECRET,
+    callbackURL: "https://rebbl.net/auth/reddit/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Reddit profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Reddit account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
 
 // set our default template engine to "ejs"
 // which prevents the need for using file extensions
@@ -28,6 +77,30 @@ app.set('view engine', 'pug');
 // set views for error and 404 pages
 app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views', "league"), path.join(__dirname, 'views', "wcq")]);
 
+
+let sessionObject = {
+  secret: 'keyboard cat'
+  , cookie: {}
+  , resave: false
+  , saveUninitialized: false
+  , store: new NedbStore({ filename: 'datastore/sessions.db' })
+};
+
+if (process.env.NODE_ENV === 'production'){
+  //app.set('trust proxy', 1) // trust first proxy
+  sessionObject.cookie.secure = true; // serve secure cookies
+  sessionObject.secret =  process.env['sessionSecret'];
+}
+
+app.use(bodyParser.urlencoded({  extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(session(sessionObject));
+
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 
 // serve static files
 app.use(express.static(path.join(__dirname, 'public-images'), {maxAge: 7*24*60*60*1000, etag: false }));
@@ -45,6 +118,33 @@ app.use('/maintenance', require('./areas/maintenance/maintenance'));
 app.use('/wcq', require('./areas/wcq/wcq'));
 
 app.use('/rebbl', require('./areas/rebbl/rebbl'));
+
+app.use('/account', require('./areas/account/account'));
+
+app.use('/signup', require('./areas/signup/signup'));
+
+
+app.get('/auth/reddit', function(req, res, next){
+  req.session.state = crypto.randomBytes(32).toString('hex');
+  req.session.save();
+  passport.authenticate('reddit', {
+    state: req.session.state,
+    duration: 'permanent'
+  })(req, res, next);
+});
+
+app.get('/auth/reddit/callback', function(req, res, next){
+  // Check for origin via state token
+  if (req.query.state == req.session.state){
+    passport.authenticate('reddit', {
+      successRedirect: req.session.returnUrl || '/',
+      failureRedirect: '/login'
+    })(req, res, next);
+  }
+  else {
+    next( new Error(403) );
+  }
+});
 
 app.get('/', function(req, res, next){
   res.redirect('/wcq');
@@ -64,5 +164,9 @@ app.use(function(req, res,next){
   res.status(404).render('404', { url: req.originalUrl });
 });
 
-app.listen(process.env.PORT);
+if (process.env.NODE_ENV === 'production'){
+  app.listen(process.env.PORT);
+} else {
+  app.listen(3000);
+}
 console.log(`Express started on port ${process.env.PORT} :: ${process.env.NODE_ENV}`);
