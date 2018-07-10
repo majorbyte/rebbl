8f586d567d2349faf4efbdae693fa150bbc8e076
majorbyte <majorbyte@gmail.com>
2018-07-10 12:36:52 +0200

    Added teams
    Now you can view teams of the coach. Not optimal yet, but I want the sync of the data live

--- areas/rebbl/team.js
+++ areas/rebbl/team.js
@@ -1,13 +1,12 @@
 'use strict';
-const db = require('../../lib/TeamService')
+const db = require('../../lib/teamservice.js')
   , util = require('../../lib/util.js')
   , express = require('express')
   , router = express.Router();
 
 router.get('/:team_id', util.checkCache, async function(req, res, next){
   let data =  await db.getTeamStats(req.params.team_id);
-
-  res.sendResponse(data);
+  res.render('rebbl/team/team', data);
 });
 
 module.exports = router;
