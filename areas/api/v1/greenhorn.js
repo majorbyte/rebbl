e63f3b016e5b379e9647319f0527b6e4c48ee1f0
majorbyte <majorbyte@gmail.com>
2018-10-06 21:42:03 +0200

    Added some more signup features

--- areas/api/v1/greenhorn.js
+++ areas/api/v1/greenhorn.js
@@ -6,7 +6,9 @@ const db = require('../../../lib/signupService.js')
 
 router.get('/', util.ensureAuthenticated, util.hasRole("admin"), async function(req, res,next){
 
-  const data = await db.getSignUps({'greenHorn':true}).all;
+  const signups = await db.getSignUps({'greenHorn':true})
+  
+  const data = signups.all;
 
   const csv = data.map(function(row){
     return `${JSON.stringify(row.team)},${JSON.stringify(row.race)},${JSON.stringify(row.timezone)},${JSON.stringify(row.reddit)},${JSON.stringify(row.discord)},${JSON.stringify(row.coach)},${JSON.stringify(row.steam)},${JSON.stringify(row.league)}`
