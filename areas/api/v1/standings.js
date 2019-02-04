16a840d7cc68932683c74b1417c0fc4fc8dc1672
majorbyte <majorbyte@gmail.com>
2019-02-04 20:56:16 +0100

    Standings csv export

--- areas/api/v1/standings.js
+++ areas/api/v1/standings.js
@@ -67,6 +67,62 @@ router.get('/stunty', util.checkCache, async function(req, res){
 
 });
 
+
+router.get('/csv/:league/:filter', util.ensureAuthenticated, util.hasRole("admin"), async function(req,res){
+
+
+  let league = req.params.league;
+  if (league.toLowerCase() !== "rebbll" && league.toLowerCase() !== "xscessively elfly league" ){
+    league = new RegExp(`^REBBL[\\s-]+${league}`, 'i');
+  } else {
+    league = new RegExp(`^${league}`, 'i');
+  }
+
+  let standings = await db.getCoachScore(league,req.params.filter,false);
+
+  standings =  standings.sort(function (a, b) {
+    if (a.competition > b.competition){
+      return 1
+    }
+    if (b.competition > a.competition){
+      return -1
+    }
+    if (a.points > b.points) {
+      return -1
+    }
+    if (b.points > a.points) {
+      return 1
+    }
+    if (a.tddiff > b.tddiff) {
+      return -1
+    }
+    if (b.tddiff > a.tddiff) {
+      return 1
+    }
+    if (a.loss > b.loss) {
+      return 1
+    }
+    if (b.loss > a.loss) {
+      return -1
+    }
+    return 0;
+  });
+
+
+  const csv = standings.map(function(row){
+    return `${JSON.stringify(row.competition || "")},${JSON.stringify(row.name)},${JSON.stringify(row.team)},${JSON.stringify(row.points)},${JSON.stringify(row.games)},${JSON.stringify(row.win)},${JSON.stringify(row.draw)},${JSON.stringify(row.loss)},${JSON.stringify(row.tddiff)}`;
+  });
+
+  csv.unshift('competition,coach,team,points,games,win,draw,loss,tddiff');
+
+  res.setHeader("content-type", "text/csv");
+  res.set('Content-Type', 'application/octet-stream');
+  res.attachment(`${req.params.league}.csv`);
+  res.status(200).send(csv.join('\r\n'));
+
+
+});
+
 router.get('/:league', util.checkCache, async function(req, res){
   let league = req.params.league;
   if (league.toLowerCase() !== "rebbll" && league.toLowerCase() !== "xscessively elfly league" ){
@@ -111,8 +167,6 @@ router.get('/:league', util.checkCache, async function(req, res){
       return 0;
     })
   );
-
-
 });
 
 module.exports = router;
