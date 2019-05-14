e2442fa4f0e951ace9cb03e16827ce0ba50add04
majorbyte <majorbyte@gmail.com>
2019-05-14 17:22:40 +0200

    Added slim version of division fixtures
    - added a slim option
    - Added gzip compression for requests

--- routes/api/v2/division.js
+++ routes/api/v2/division.js
@@ -37,6 +37,31 @@ class DivisionApi{
       }
     });  
 
+    this.router.get('/:league/:season/:division/slim', util.checkCache, async function(req, res){
+      try {
+        let {league,season, division} = req.params;
+
+        let data = await dataService.getSchedules({league:league,season:season,competition:division});
+
+        let ret = data.map(m => {
+          let homeTeam = m.opponents[0].team.name;
+          let homeScore = m.opponents[0].team.score;
+          let awayTeam = m.opponents[1].team.name;
+          let awayScore = m.opponents[1].team.score;
+          let round = m.round;
+
+          return {round,homeTeam,homeScore,awayScore,awayTeam};
+        })
+
+        res.json(ret.sort((a,b)=> a.round > b.round ? 1 : -1));
+      }
+      catch (ex){
+        console.error(ex);
+        res.status(500).send('Something something error');
+      }
+    }); 
+
+
     this.router.get('/:league/:season/:division/:round', util.checkCache, async function(req, res){
       try {
         let {league,season, division, round} = req.params;
