e73b2ac9a66614e8828828c3d5a511bf7c3ba0f8
majorbyte <majorbyte@gmail.com>
2019-07-18 17:01:14 +0200

    Added list of legendary players

--- routes/bloodbowl/bloodbowl.js
+++ routes/bloodbowl/bloodbowl.js
@@ -10,7 +10,8 @@ class BloodBowl{
 
 
   routesConfig(){
-    this.router.get('/starplayers',/*util.checkCache,*/function(req, res){res.render('bloodbowl/starplayers');});
+    this.router.get('/starplayers',util.checkCache,function(req, res){res.render('bloodbowl/starplayers');});
+    this.router.get('/legendaryplayers',util.checkCache,function(req, res){res.render('bloodbowl/legendaryplayers');});
 
     return this.router;
   }
