dc140067629a4f27ab1093d5b4796ac3b2e575ee
majorbyte <majorbyte@gmail.com>
2019-09-25 17:48:38 +0200

    Added refresh competition option for admin
    Instead of using me needed to run a script when a rampup coach gets moved up, admins can do it themselves.
    
    Fixed skilldescription problem, closes #497

--- routes/api/v1/admin/divisions.js
+++ routes/api/v1/admin/divisions.js
@@ -3,6 +3,7 @@
 const express = require("express")
   , api = require("../../../../lib/apiService.js")
   , dataService = require("../../../../lib/DataService").rebbl
+  , maintenanceService = require("../../../../lib/MaintenanceService.js")
   , util = require("../../../../lib/util.js")
   , router = express.Router();
 
@@ -12,6 +13,11 @@ const express = require("express")
     try{
       
       let data = await dataService.getSchedules({season:"season 12", round:1, league:{$in:["REBBL - GMan","REBBL - Big O", "REBBL - REL"]}},{projection:{_id: 0,league:1, competition:1, competition_id:1}});
+      let upstarts =  await dataService.getSchedules({season:"season 13", round:1, league:"ReBBRL Upstarts"},{projection:{_id: 0,league:1, competition:1, competition_id:1}});
+      let minors =  await dataService.getSchedules({season:"season 8", round:1, league:"ReBBRL Minors League"},{projection:{_id: 0,league:1, competition:1, competition_id:1}});
+      let college =  await dataService.getSchedules({season:"season 9", round:1, league:"ReBBRL College League"},{projection:{_id: 0,league:1, competition:1, competition_id:1}});
+
+      data = data.concat(upstarts,minors,college);
 
       let temp = [...new Set(data.map(x=> x.competition_id))];
       data = temp.map(x => data.find(d => d.competition_id === x));
@@ -34,6 +40,17 @@ const express = require("express")
       })
 
       res.status(200).send(data);
+    } catch(err){
+      console.log(err);
+    }
+  });
+
+  router.get("/refresh/:league/:competition", util.ensureAuthenticated, util.hasRole("admin"), async function(req, res){
+    try{
+
+      maintenanceService.initRebblData(req.params.league, req.params.competition)
+
+      res.status(200).send();
     } catch(err){
       console.log(err);
     }
