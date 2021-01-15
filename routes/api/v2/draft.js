e63bee27aac0e5e506c964dc04108da95e09f98a
majorbyte <majorbyte@gmail.com>
2021-01-15 23:44:18 +0100

    Added option for clan admin to rerun draft steps

--- routes/api/v2/draft.js
+++ routes/api/v2/draft.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express')
+  , dataService = require("../../../lib/DataService.js").rebbl
   , draftService = require("../../../lib/DraftService.js")
   , logging = require("../../../lib/loggingService.js")
   , util = require('../../../lib/util.js');
@@ -38,8 +39,38 @@ class DraftApi{
     }
   }
 
+  async completeDraftProcess(req,res){
+
+    const query = {
+      house: Number(req.params.house),
+      round: Number(req.params.round), 
+      competition: req.params.division, 
+      season: req.params.season
+    };
+
+    const draft = await dataService.getDraft(query);
+
+    switch(req.params.part){
+      case "powers":
+        await draftService.registerPowers(draft);
+        break;
+      case "matches":
+        const unstarted = await draftService.createGames(draft,data);
+
+        dataService.updateSchedule(query, {$set:{unstarted: unstarted, drafted:true}});
+        break;
+      case "post":
+        await draftService.postToReddit(draft);
+        break;
+    }
+
+  }
+
   routesConfig(){
     this.router.get("/:division/:round/:house", util.ensureAuthenticated, this.getDraftData);
+
+    this.router.put("/:season/:division/:round/:house/:part", util.ensureAuthenticated, util.hasRole("admin","clanadmin"), this.completeDraftProcess);
+
     this.router.get("/", util.ensureAuthenticated, this.getDraftData);
  
     this.router.post("/", util.ensureAuthenticated, util.hasRole("clanleader"), this.saveDraftData);
