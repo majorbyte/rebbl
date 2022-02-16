f6d09060d0623408774987f129db3a2b234ffd17
majorbyte <majorbyte@gmail.com>
2022-02-16 22:13:52 +0100

    to amend

--- routes/api/v2/clanBuilding.js
+++ routes/api/v2/clanBuilding.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const express = require("express")
-, accountService = require("../../../lib/accountService.js")
-, clanService = require("../../../lib/ClanService.js")
-, rateLimit = require("express-rate-limit")
-, util = require('../../../lib/util.js')
+const express = require('express')
+, accountService = require('../../../lib/accountService.js')
+, clanService = require('../../../lib/ClanService.js')
+, rateLimit = require('express-rate-limit')
+, util = require('../../../lib/util.js');
+const { getActiveClanSeason } = require('../../../lib/ConfigurationService.js');
 
 class ClanBuildingApi{
   constructor(){
@@ -15,13 +16,13 @@ class ClanBuildingApi{
   team(){
     return {
       roster:[],
-      name:""
+      name:''
     }
   };
   
   async _getCoach(req,res){
     const account = await accountService.searchAccount({coach:new RegExp(`^${req.params.coach}$`,'i')});
-    if (!account) res.status(404).json("coach not found");
+    if (!account) res.status(404).json('coach not found');
     else res.json({reddit: account.reddit, coach:account.coach, discord:account.discord});
   }
 
@@ -46,26 +47,53 @@ class ClanBuildingApi{
 
   async _getClan(req,res){
     const account = await accountService.getAccount(req.user.name);
-    let clan = await clanService.getNewClanByUser(account.coach); 
+    let clan = await clanService.getNewClanByUser(account.coach);
     res.json(clan);
   }
+
+  async _registerClan(req,res){
+    const account = await accountService.getAccount(req.user.name);
+    const clan = clanService.createClan(req.params.clan, {
+      coach: account.coach,
+      coachId: account.coachId || 0,
+      reddit: account.reddit,
+      discord: account.discord
+    });
+    res.json(clan);
+  }
+
+  async _saveTeam(req,res){
+    res.json({});
+  }
+
+
+  async teamSaveAllowed(req, res, next) {
+    const account = await accountService.getAccount(req.user.name)
+    return account.roles?.includes("clanleader") || account.coach.toLowerCase() === req.body.coach.toLowerCase() 
+      ? next() 
+      : res.status(403).send({error: "You are not allowed to make changes to this team"});
+  };
 
   routesConfig(){
     const apiRateLimiter = rateLimit({
       windowMs: 30 * 1000,
       max: 2, 
       message:
-        "Too many requests, please wait 30 seconds",
+        'Too many requests, please wait 30 seconds',
       keyGenerator: function(req){
         return `${req.ip}+${req.user.name}+clan_coach`;
       }
     });
 
-    this.router.use("/coach/:coach/team",util.ensureAuthenticated , apiRateLimiter, this._getReturningTeam.bind(this));
-    this.router.use("/coach/:coach",util.ensureAuthenticated ,  this._getCoach.bind(this));
-    this.router.use("/team/:teamId/players",util.ensureAuthenticated ,  this._getReturningTeamPlayers.bind(this));
-    this.router.use("/:clan/:team",util.ensureAuthenticated ,  this._getTeam.bind(this));
-    this.router.use("/", this._getClan.bind(this))
+    this.router.get('/coach/:coach/team',util.ensureAuthenticated , apiRateLimiter, this._getReturningTeam.bind(this));
+    this.router.get('/coach/:coach',util.ensureAuthenticated ,  this._getCoach.bind(this));
+    this.router.get('/team/:teamId/players',util.ensureAuthenticated ,  this._getReturningTeamPlayers.bind(this));
+    this.router.get('/:clan/:team',util.ensureAuthenticated ,  this._getTeam.bind(this));
+    this.router.get('/', this._getClan.bind(this));
+
+    this.router.post('/:clan', util.hasRole("clanleader"), this._registerClan.bind(this));
+
+    this.router.put('/:clan/team', this.teamSaveAllowed, this._saveTeam.bind(this));
 
 
     return this.router;
