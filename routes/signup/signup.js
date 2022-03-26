52889b35d1c5b7eb537eea1c6342d6169f5f08e1
majorbyte <majorbyte@gmail.com>
2022-03-26 20:24:28 +0100

    Closing signups

--- routes/signup/signup.js
+++ routes/signup/signup.js
@@ -58,7 +58,7 @@ class Signup{
     
     //this.router.post('/confirm-rampup',util.ensureLoggedIn, this._confirmRampup.bind(this));
 
-    this.router.get('/change', util.ensureLoggedIn, this._changeSignup.bind(this));
+    /*this.router.get('/change', util.ensureLoggedIn, this._changeSignup.bind(this));
 
     this.router.post('/resign', util.ensureAuthenticated, this._resign);
   
@@ -74,7 +74,7 @@ class Signup{
 
     this.router.post('/confirm-greenhorn', util.ensureAuthenticated, this._confirmGreenhornCup);
 
-    this.router.post('/resign-greenhorn', util.ensureAuthenticated, this._resignGreenhornCup);
+    this.router.post('/resign-greenhorn', util.ensureAuthenticated, this._resignGreenhornCup);*/
 
     this.router.get('/signups/rebbrl', util.cache(10*60), function(req,res){res.render('signup/signups');});
   
@@ -84,8 +84,8 @@ class Signup{
     this.router.get('/rebbrl/college', util.ensureLoggedIn, this._college.bind(this));
    // this.router.get('/rebbrl/college-reserves', util.ensureLoggedIn, this._collegeReserve.bind(this));
     this.router.post('/confirm-new-rebbrl', util.ensureLoggedIn, this._confirmRebbrl.bind(this));
-    this.router.get('/rebbrl/minors', util.ensureLoggedIn, this._minors.bind(this));
-    this.router.post('/resign-rebbrl', util.ensureLoggedIn, this._resignRebbrl);
+//    this.router.get('/rebbrl/minors', util.ensureLoggedIn, this._minors.bind(this));
+ //   this.router.post('/resign-rebbrl', util.ensureLoggedIn, this._resignRebbrl);
     
     return this.router;
   }
