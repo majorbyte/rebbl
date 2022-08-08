b1ffbb3aa8bddac90ad9a52b41506ef52527a7c7
majorbyte <majorbyte@gmail.com>
2022-08-08 21:59:15 +0200

    College Reserves

--- routes/signup/signup.js
+++ routes/signup/signup.js
@@ -45,17 +45,17 @@ class Signup{
 
   routesConfig(){
     
-   this.router.get('/', async function(req, res){
+/*   this.router.get('/', async function(req, res){
       res.render('signup/closed');
    });
-/*
+*/
     this.router.get('/', util.ensureAuthenticated, this._getStatus);
 
     this.router.get('/discord', util.ensureAuthenticated, this._authDiscord);
     this.router.get('/nodiscord', util.ensureAuthenticated, this._noDiscord);
 
     this.router.get('/discord/callback', this._authDiscordCallback);
-    
+ /*   
     //this.router.post('/confirm-rampup',util.ensureLoggedIn, this._confirmRampup.bind(this));
 
     this.router.get('/change', util.ensureLoggedIn, this._changeSignup.bind(this));
@@ -82,7 +82,7 @@ class Signup{
     this.router.get('/counter', async function(req, res){res.render('signup/counter');});
 
     //this.router.get('/rebbrl/college', util.ensureLoggedIn, this._college.bind(this));
-    //this.router.get('/rebbrl/college-reserves', util.ensureLoggedIn, this._collegeReserve.bind(this));
+    this.router.get('/rebbrl/college-reserves', util.ensureLoggedIn, this._collegeReserve.bind(this));
     
     //this.router.post('/confirm-new-rebbrl', util.ensureLoggedIn, this._confirmRebbrl.bind(this));
     //this.router.get('/rebbrl/minors', util.ensureLoggedIn, this._minors.bind(this));
