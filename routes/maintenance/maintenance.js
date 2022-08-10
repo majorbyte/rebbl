4c1815a93542d29e323144ec9aad1d646c71b09d
majorbyte <majorbyte@gmail.com>
2022-08-10 20:34:42 +0200

    Sync update

--- routes/maintenance/maintenance.js
+++ routes/maintenance/maintenance.js
@@ -129,7 +129,7 @@ class Maintenance{
       if (req.app.locals.cyanideEnabled){
         try{
           await maintenanceService.getRebblData(req.query.league);
-          await ts.checkTickets();
+          //await ts.checkTickets();
         }
         catch(e){
           loggingService.error(e);
