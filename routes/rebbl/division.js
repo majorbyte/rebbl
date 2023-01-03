a57d1cd70ba477c1cb0454df5b6e9be294ec95e6
majorbyte <majorbyte@gmail.com>
2023-01-03 21:56:11 +0100

    Season 21 startup

--- routes/rebbl/division.js
+++ routes/rebbl/division.js
@@ -53,7 +53,7 @@ class Division{
     if( req.params.league.toLowerCase() === "rampup"){
       leagueRegex = new RegExp(`${league}$`, 'i');
       divRegex = new RegExp(`^${req.params.division}`, 'i');
-      season = "season 20";
+      season = "season 21";
     } 
     
     if (season !== "")
@@ -111,7 +111,7 @@ class Division{
       if( req.params.league.toLowerCase() === "rampup"){
         leagueRegex = new RegExp(`${league}$`, 'i');
         divRegex = new RegExp(`^${req.params.division}`, 'i');
-        season = "season 20";
+        season = "season 21";
       }
 
       if (season !== "")
