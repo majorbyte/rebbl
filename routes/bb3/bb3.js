218d7bc59d68420029c92e8485bffed9c3fff045
majorbyte <majorbyte@gmail.com>
2023-03-22 21:22:47 +0100

    More bb3 fixes

--- routes/bb3/bb3.js
+++ routes/bb3/bb3.js
@@ -15,7 +15,8 @@ class BB3{
 
   team = async (req,res) => {
     const team = await dataService.getTeam({id:req.params.id});
-    const matches = await dataService.getMatches({matchId:{$in:team.matches}});
+    const m = Array.isArray(team?.matches) ? team.matches : [];
+    const matches = await dataService.getMatches({matchId:{$in:m}});
     return res.render("bb3/fullTeam", {team, matches, user:res.locals.user});
   };
 
