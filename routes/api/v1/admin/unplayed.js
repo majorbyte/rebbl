f6e15c057726c6fcec146e3972d8752b389d0ba8
majorbyte <majorbyte@gmail.com>
2024-04-02 16:09:30 +0200

    Repurposed /admin/unplayed for bb3

--- routes/api/v1/admin/unplayed.js
+++ routes/api/v1/admin/unplayed.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const express = require('express')
-  , redditService = require("../../../../lib/RedditService.js")
-  , maintenanceService = require("../../../../lib/MaintenanceService.js")
+  , dataService = require("../../../../lib/DataServiceBB3.js").rebbl3
   , util = require('../../../../lib/util.js')
   , router = express.Router();
 
@@ -11,7 +10,14 @@ const express = require('express')
   router.get('/', util.ensureAuthenticated, util.hasRole("admin"), async function(req, res){
     try{
       
-      let data = await redditService.getUnplayedGames();
+
+      let competitions = await dataService.getCompetitions({leagueId:"94f0d3aa-e9ba-11ee-a745-02000090a64f"});
+      let data = [];
+      for(const competition of competitions){
+        const schedules = await dataService.getSchedules({competitionId:competition.id, status:{$lt:3}});
+        schedules.forEach(x => x.competition = competition.name);
+        data = data.concat(schedules);
+      }
 
       res.status(200).send(data);
     } catch(err){
@@ -19,17 +25,4 @@ const express = require('express')
     }
   });
   
-  router.post('/:contest_id', util.ensureAuthenticated, util.hasRole("admin"), async function(req, res){
-    try{
-  
-      await maintenanceService.fixAdminGames(req.params.contest_id);
-
-      res.status(200).send("ok");
-    } catch(err){
-      console.log(err);
-    }
-  });
-
-
-
 module.exports = router;
