03420be37fbd7b3a3775cd4fb86b2eb2bd6c3e65
majorbyte <majorbyte@gmail.com>
2024-04-30 22:19:39 +0200

    Redraft GUI

--- routes/bb3/redraft.js
+++ routes/bb3/redraft.js
@@ -12,8 +12,12 @@ class Redraft{
 		this.router = express.Router();
 	}
 
+  teamView = async (req,res) => {
 
-  team = async (req,res) => {
+    return res.render("bb3/redraft/index", {id:req.params.teamId});
+  }
+
+  teamData = async (req,res) => {
     
     const team = await redraftService.getRedraftTeam(req.params.teamId);
     const retiredPlayers = await dataService.getRetiredPlayers({teamId:req.params.teamId});
@@ -23,11 +27,22 @@ class Redraft{
     const race = races.find(x => x.code == team.race);
 
     const allowedPositions = positions.filter(x => x.race === race.prefix);
-    return res.render("bb3/redraft/index", {team, retiredPlayers, allowedPositions});
+    delete team.matches;
+    delete team.primaryColor;
+    delete team.secondaryColor;
+    delete team.tertiaryColor;
+    delete team.jerseyPattern;
+    team.roster.forEach(player => {
+      delete player.equipmentIds;
+    });
+
+    res.json({team, retiredPlayers, allowedPositions});
   }
 
   routesConfig(){
-    this.router.get("/:teamId", util.cache(1), util.checkAuthenticated, this.team);
+    this.router.get("/:teamId", util.cache(1), util.checkAuthenticated, this.teamView);
+    
+    this.router.get("/api/:teamId", util.cache(1),  this.teamData);
 
     return this.router;
   }
