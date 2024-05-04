01c1436ba8eaea668771a8b4f641acf1fe9880cb
majorbyte <majorbyte@gmail.com>
2024-05-04 21:56:14 +0200

    WIP

--- routes/bb3/redraft.js
+++ routes/bb3/redraft.js
@@ -12,19 +12,37 @@ class Redraft{
 		this.router = express.Router();
 	}
 
-  teamView = async (req,res) => {
+  #teamView = async (req,res) => {
 
     return res.render("bb3/redraft/index", {id:req.params.teamId});
   }
 
-  startRedraft = async (req,res) => {
+  #startRedraft = async (req,res) => {
     const result =  await redraftService.startRedraft(req.params.teamId, res.locals.user);
 
     if (result?.error) res.status(400).json(result);
     else res.json({redirect: `/bb3/redraft/${req.params.teamId}`});
   }
 
-  teamData = async (req,res) => {
+  #draftPlayer = async (req,res) =>  {
+    try{
+      await redraftService.draftPlayer(req.params.teamId, req.params.playerId, res.locals.user);
+      res.status(200).json();
+    } catch (ex) {
+      res.status(400).json(ex);
+    }
+  }
+  #undraftPlayer = async (req,res) => {
+    try{
+      await redraftService.undraftPlayer(req.params.teamId, req.params.playerId, res.locals.user);
+      res.status(200).json();
+    } catch (ex) {
+      res.status(400).json(ex);
+    }
+  }
+
+
+  #teamData = async (req,res) => {
     
     const team = await dataService.getTeam({id: req.params.teamId});
     const retiredPlayers = await dataService.getRetiredPlayers({teamId:req.params.teamId});
@@ -38,12 +56,16 @@ class Redraft{
     res.json({team:team.redraft, retiredPlayers, allowedPositions});
   }
 
-  routesConfig(){
-    this.router.get("/:teamId", util.cache(1), util.checkAuthenticated, this.teamView);
-    
-    this.router.post("/api/:teamId", util.ensureAuthenticated, this.startRedraft)
 
-    this.router.get("/api/:teamId", util.cache(1),  this.teamData);
+  routesConfig(){
+    this.router.get("/:teamId", util.cache(1), util.checkAuthenticated, this.#teamView);
+    
+    this.router.post("/api/:teamId", util.ensureAuthenticated, this.#startRedraft);
+
+    this.router.put("/api/:teamId/:playerId", util.ensureAuthenticated, this.#draftPlayer);
+    this.router.delete("/api/:teamId/:playerId", util.ensureAuthenticated, this.#undraftPlayer);
+
+    this.router.get("/api/:teamId", util.cache(1),  this.#teamData);
 
 
     return this.router;
