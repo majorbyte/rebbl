dc803404cd04cd5071c7d185847573fce0c2aac0
Martin Sjögren <msjogren@gmail.com>
2024-06-10 21:35:50 +0200

    Clan S18 (aka the swissening) prep

--- routes/api/v2/clan.js
+++ routes/api/v2/clan.js
@@ -320,7 +320,7 @@ class ClanApi{
 
     this.router.get("/data", util.ensureAuthenticated, util.hasRole("admin"),async (req,res) => {
       
-      const schedules = await dataService.getSchedules({league:"clan", season:"season 17"});
+      const schedules = await dataService.getSchedules({league:"clan", season:"season 18"});
       
       const data = [];
 
