8d0490b96382ed45a78cc91dd56fa320255221c8
majorbyte <majorbyte@gmail.com>
2025-03-03 21:02:18 +0100

    Added offseason templates

--- routes/api/v1/admin/templates.js
+++ routes/api/v1/admin/templates.js
@@ -3,12 +3,22 @@
 const express = require('express')
   , dataService = require("../../../../lib/DataService.js").rebbl
   , reddit = require("../../../../lib/RedditService.js")
+  , scheduleTemplates = require("../../../../lib/ScheduleTemplates.js")
   , util = require('../../../../lib/util.js')
   , router = express.Router();
 
 router.get("/", util.ensureAuthenticated, util.hasRole("admin"), async function(req,res){
   res.json(await dataService.getScheduleTemplates());
 });
+
+router.get("/:key", util.ensureAuthenticated, util.hasRole("admin"), async function(req,res){
+  const template = await dataService.getScheduleTemplate({key:req.params.key});
+
+  const result =  await scheduleTemplates.getTemplate(template, Date.now);
+
+  res.send(result);
+});
+
 
 router.post("/:key", util.ensureAuthenticated, util.hasRole("admin"), async function(req,res){
   const template = await dataService.getScheduleTemplate({key:req.params.key});
